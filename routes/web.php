98de4cec21ab0941f42a725f3d62cd316e075b1e
azaharul-just <47347012+azaharul-just@users.noreply.github.com>
2021-03-12 19:33:10 +0600

    User Dashboard Added login and logout

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\MainUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,5 +29,10 @@ Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', f
 })->name('dashboard');
 
 Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    return view('user.index');
 })->name('dashboard');
+
+
+//User Backend Operations
+
+Route::get('/user/logout',[MainUserController::class,'Logout'])->name('user.logout');
